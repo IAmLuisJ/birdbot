e0a651f05afe851fa90ae0210b719f53130ddb29
Luis Juarez <luisisthejuarez@gmail.com>
2021-11-20 13:50:21 -0500

    testing other functions

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const client = new Client();
 
 client.on("ready", async () => {
     //callback function that gets called when the client is ready to use
-  //  const followerList = await client.users.fetchFollowers({ username: 'helloluisj'}, 5)
+ 
     const user = await client.users.fetchByUsername({username: 'helloluisj'});
     console.log(user);
+
+    const followerList = await client.users.fetchFollowers(user, 5)
+    console.log(followerList);
 })
 
 client.loginWithBearerToken(Bearer);
