612cecc7a041eaee1fcf25190f723430f0e41428
Luis Juarez <luisisthejuarez@gmail.com>
2021-11-19 23:50:45 -0500

    fetch

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-import {Client} from 'twitter.js';
+import {Client, UserBaseEntity, UserManager} from 'twitter.js';
+import Bearer from './secrets.js';
 
 const client = new Client();
 
-console.log(client.readyAt);
+client.on("ready", async () => {
+    //callback function that gets called when the client is ready to use
+  //  const followerList = await client.users.fetchFollowers({ username: 'helloluisj'}, 5)
+    const user = await client.users.fetchByUsername({username: 'helloluisj'});
+    console.log(user);
+})
 
+client.loginWithBearerToken(Bearer);
